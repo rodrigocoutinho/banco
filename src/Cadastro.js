ec5cda18b344177b3a1957dd15dc3c998b700b0e
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2021-08-28 23:55:14 -0300

    Atualizando

---
+++ src/Cadastro.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from 'react';
+import './Cadastro.css';
+import * as S from './styled';
+
+export default function Cadastro() {
+
+  return (
+    <>
+
+      <S.Header>
+
+        <div>
+          <h1>JobsNet</h1>
+        </div>
+        <h1>Banco de Talentos</h1>
+
+      </S.Header>
+
+      <S.Title> Dados Pessoais</S.Title>
+      <S.Content className="Cadastro">
+
+        <div>
+
+          <label for="nomeCompleto">
+            <S.SubTitle>Nome Completo: <span>*</span></S.SubTitle>
+          </label>
+          <S.Input type="text" id="nomeCompleto" className="nomeCompleto" placeholder="Digite seu nome" required></S.Input>
+
+          <label for="cargo">
+            <S.SubTitle>Cargo Pretendido: </S.SubTitle>
+          </label>
+          <S.Input type="text" id="cargo" className="cargo" placeholder="ex. Desenvolvedor Front-End"></S.Input>
+
+          <label for="email">
+            <S.SubTitle>E-mail: </S.SubTitle>
+          </label>
+          <S.Input type="email" id="email" className="email" ></S.Input>
+
+        </div>
+
+        <div>
+
+          <label for="nascimento">
+            <S.SubTitle> Data de Nascimento: <span>*</span></S.SubTitle>
+          </label>
+          <S.Input type="date" id="nascimento" className="nascimento" placeholder="ex. Desenvolvedor Front-End" required></S.Input>
+
+          <label for="cpf">
+            <S.SubTitle> CPF: <span>*</span></S.SubTitle>
+          </label>
+          <S.Input type="text" id="cpf" className="cpf" placeholder="Apenas numeros" required></S.Input>
+
+          <label>
+          <S.SubTitle> Estado civil: <span>*</span></S.SubTitle>
+            <S.Selecao id="estadoCivil">
+              <option> </option>
+              <option>Solteiro</option>
+              <option>Casado</option>
+            </S.Selecao>
+          </label>
+
+
+
+
+        </div>
+        <div>
+          
+          <label>
+            <S.SubTitle> Sexo: <span>*</span></S.SubTitle>
+            <S.Selecao id="sexo">
+              <option> </option>
+              <option>Masculino</option>
+              <option>Feminino</option>
+              <option>Não declarar</option>
+            </S.Selecao>
+
+          </label>
+          <label>
+
+            <S.SubTitle> Possui CNH: </S.SubTitle>
+            <S.Selecao id="cnh">
+              <option> </option>
+              <option value="sim">Sim</option>
+              <option value="nao">Não</option>
+            </S.Selecao>
+          </label>
+          <label for="rg">
+            <S.SubTitle>RG: <span>*</span></S.SubTitle>
+          <S.Input type="text" id="rg" className="rg"></S.Input>
+          </label>
+          
+        </div>
+
+        <div>
+          <label for="endereco">
+            <S.SubTitle>Endereço: <span>*</span></S.SubTitle>
+          <S.Input type="text" id="endereco" className="endereco" placeholder="ex. Avenida Tiradentes" required></S.Input>
+          </label>
+
+          <label for="cidade">
+            <S.SubTitle>Cidade: <span>*</span></S.SubTitle>
+          <S.Input type="text" id="cidade" className="cidade" placeholder="ex. Goiânia" required></S.Input>
+          </label>
+
+          <label for="estado">
+            <S.SubTitle>Estado: <span>*</span></S.SubTitle>
+          <S.Input type="text" id="estado" className="estado" placeholder="ex. GO" required></S.Input>
+          </label>
+        </div>
+
+        <div>
+          <label for="cep">
+            <S.SubTitle>CEP: <span>*</span></S.SubTitle>
+          <S.Input type="text" id="cep" className="cep" required></S.Input>
+          </label>
+
+          <label for="bairro">
+            <S.SubTitle>Bairro: <span>*</span></S.SubTitle>
+            <S.Input type="text" id="bairro" className="bairro"></S.Input>
+          </label>
+          
+          <label for="telefone">
+            <S.SubTitle>Telefone: <span>*</span></S.SubTitle>
+            <S.Input type="text" id="telefone" className="telefone"></S.Input>
+          </label>
+
+        </div>
+
+      </S.Content>
+
+    </>
+  )
+}
---
+++ src/styled.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+export const Body = styled.body`
+    background-image: url("/img/pexels-photo-5011647.jpeg");
+    background-repeat: none;
+    background-position: center center fixed;
+    background-size: cover;
+`
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    /* border-bottom: 1px solid crimson; */
+    box-shadow: 1px 1px 10px 1px burlywood;
+    border-bottom: 15px ridge crimson;
+    background-color: darkgray;
+`
+export const Ul = styled.ul`
+display: flex;
+`
+
+export const Title = styled.h1`
+    color: blueviolet;
+    text-align: center;
+`
+
+export const SubTitle = styled.h3`
+    //color: #f00;
+    //text-align: center;
+    //justify-content: center;
+`
+
+export const Selecao = styled.select`
+    //color: #f00;
+    //text-align: center;
+    //align-items: center;
+    padding: 5px 10px;
+    border-radius: 10px;
+    //margin-left: 50px;
+    //width: 50%;
+    
+
+    &:focus,
+    &:active{
+        outline: none;
+        box-shadow: none;
+    }
+`
+
+export const Home = styled.div`
+    //display: flex;
+    //flex-direction: column;
+    //align-items: center;
+    //justify-content: center;
+    height: 100vh;
+
+    background-image: url("pexels-photo-5011647.jpeg");
+    background-repeat: none;
+    background-position: center center fixed;
+    background-size: cover;
+    //background-color: darkgoldenrod;
+    
+`
+export const Content = styled.div`
+    //width: 100vw;
+    display: flex;
+    //align-items: center;
+    //justify-content: center;
+    width: 70%;
+    margin: 0 auto;
+    border: thick double rgb(21, 255, 0);
+    padding: 25px;
+    background-color: darkgray;
+
+    border-radius: 20px;
+  
+
+    &:focus,
+    &:active{
+        outline: none;
+        box-shadow: none;
+    }
+`
+
+
+export const Input = styled.input`
+    //margin-left: 5px;
+    //width: 50%;
+    padding: 5px 10px;
+    border-radius: 10px;
+    margin: auto 10px;
+
+    &:focus,
+    &:active{
+        outline: none;
+        box-shadow: none;
+    }
+`
+
+export const ErrorMsg = styled.span`
+    display: block;
+    font-size: 0.65rem;
+    color: red;
+    font-weight: 600;
+    margin-top: 1rem;
+
+`
